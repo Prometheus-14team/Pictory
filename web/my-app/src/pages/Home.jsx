de33c03523d9d5be97e213acdd8336bd9bca93ea
bydayoung <pdysicist@naver.com>
2025-01-05 16:43:36 +0900

    home

--- web/my-app/src/pages/Home.jsx
+++ web/my-app/src/pages/Home.jsx
@@ -15,14 +15,32 @@ const Home = () => {
     <div className="intro">
       <div className='jung'>
         <div className="rectangle" />
+      </div>
+      <div className="smile">
+          <img alt="Group" src={group12} />
+      </div>
+      <div className='jung'>
         <div className="ellipse" />
         <img className="group" alt="Group" src={group1} />
-        <img className="group-2" alt="Group" src={group15} />
-        <img className="vector" alt="Vector" src={vector316} />
         <div className="flexcontainer">
           <p className="text">
             <span className="span">
               PICTORY, 당신의 내면 이야기를 그림으로 펼쳐내는 마법 같은 공간
+              <br />
+            </span>
+          </p>
+          <p className="text">
+            <span className="span">
+              <br />
+            </span>
+          </p>
+          <p className="text">
+            <span className="span">
+              <br />
+            </span>
+          </p>
+          <p className="text">
+            <span className="span">
               <br />
             </span>
           </p>
@@ -49,31 +67,22 @@ const Home = () => {
           </p>
           <p className="text">
             <span className="span">
+              <br />
+            </span>
+          </p>
+          <p className="text">
+            <span className="span">
+              <br />
+            </span>
+          </p>
+          <p className="text">
+            <span className="span">
               글로 적은 당신의 내밀한 이야기를 AI가 섬세하게 읽어내고, <br /><br />
               마치 어린 시절 상상 속 그림책처럼 생생하게 시각화합니다.
             </span>
           </p>
         </div>
-        <div className="box">
-          <img alt="Group" src={group11} />
-        </div>
-        <div className="pencil">
-          <img className="vector" alt="Group" src={group13} />
-        </div>
-        <div className="smile">
-          <img className="vector" alt="Group" src={group12} />
-        </div>
-        <div className="div">
-          <p className="text">
-              <span className="span">
-                <br />
-              </span>
-          </p>
-          <p className="text">
-              <span className="span">
-                <br />
-              </span>
-          </p>
+        <div className="flexcontainer2">
           <p className="text">
             <span className="span">
               매 순간의 이야기를 고유한 화풍으로 담아내는 당신만의
@@ -88,12 +97,20 @@ const Home = () => {
             </span>
           </p>
         </div>
-        <div className="text-wrapper"><Link to="/calendar">Start here</Link></div>
-        <img className="group-5" alt="Group" src={group2} />
-        <img className="vector-2" alt="Vector" src={vector306} />
       </div>
+      <div className="text-wrapper"><Link to="/calendar">Start here</Link></div>
+      <img className="group-5" alt="Group" src={group2} />
+      <img className="vector-2" alt="Vector" src={vector306} />
       <div className="text-wrapper-2">Ai drawing diary</div>
       <img className="group-6" alt="Group" src={group2} />
+      <img className="group-2" alt="Group" src={group15} />
+      <img className="vector" alt="Vector" src={vector316} />
+      <div className="box">
+          <img alt="Group" src={group11} />
+      </div>
+      <div className="pencil">
+          <img alt="Group" src={group13} />
+      </div>
     </div>
   );
 };
